c19dde75bab59e2f9e2eaa4c7cc2e03f333f0872
3RaghavK3 <33raghavk33@gmail.com>
2025-11-19 18:39:43 +0530

    :responsive banner and header

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -4,25 +4,26 @@ import banneritem from "../assets/banneritem2.png";
 export function Banner() {
   return (
     <>
-      <div className="w-full h-[400px] flex items-center bg-[#F2F2FD] px-40">
-        <div className="h-[70%]flex flex-col justify-between flex-1">
-          <span className="text-lg">Absolutely hot collections</span>
-          <div className="text-5xl  leading-[1.15] gap-10">
+      <div className="w-full flex flex-col  items-center md:flex-row items-center  bg-[#F2F2FD] p-5 md:p-10 md:px-15 lg:px-40" >
+        <div className="h-[70%]flex flex-col text-center md:text-left  flex-1">
+          <span className="text-sm md:text-base lg:text-xl">Absolutely hot collections</span>
+          <div className="text-2xl md:text-4xl lg:text-6xl leading-[1.15] gap-10">
             <div className="font-semibold">The Best Place To</div>
             <div className="font-semibold"> Find And Buy</div>
             <div className="font-bold">
               Amazing <span className="text-primary">Product</span>
             </div>
                       
-          <div className="w-fit text-2xl p-1 px-3 rounded-sm text-white overflow-y-hidden bg-accent mt-4">Browse our collections below</div>
+          <div className="w-fit text-lg md:text-2xl lg:text-4xl p-1 px-3 rounded-sm text-white overflow-y-hidden bg-accent mt-4 ">Browse our collections below</div>
 
           </div>
         </div>
 
-        <div className="h-[100%] flex-1  ">
+        <div className="h-[100%] flex-1">
           <img src={banneritem} className="w-full h-full object-contain" />
         </div>
       </div>
+      
     </>
   );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,17 +3,17 @@ export function Header() {
   
   return (
     <>
-    <div className="bg-accent overflow-x-hidden">
-      <div className="justify-around relative items-center flex text-white animate-marquee-reverse"> 
+    <div className="bg-accent text-sm md:text-base w-full overflow-x-hidden">
+      <div className="relative items-center text-white flex whitespace-nowrap p-1 gap-10 md:gap-20 lg:gap-30 animate-marquee-reverse w-fit"> 
         <div>Click on the profile icon to view your past orders.</div>
         <div>💥 Enjoy up to 50% OFF on your favorite collections!</div>
         <div>
           View your cart anytime by clicking the cart icon.
-          </div>
+        </div>
       </div>
     </div>
     
-    <div className="flex justify-center h-20 border-2">
+    <div className="flex justify-center h-15 md:h-18 lg:h-20 border-2">
        <img src={logo} className="object-contain"/>
     </div>
 
